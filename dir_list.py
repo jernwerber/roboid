60fe6eda434e3436722408b38573b867e94fd3d3
jernwerber <jon.weber@gmail.com>
2018-08-21 22:25:49 -0400

    Update dir_list.py

--- dir_list.py
+++ dir_list.py
@@ -1,12 +1,41 @@
-# on initialize, look for a usb device that has a folder called sounds
-# or we can give it a path... for the rpi it'll probably be /dev/sda or something like that
+import os
 
 # specify valid file formats, based on file extensions (easiest way?)
 playable_formats = ['mp3','ogg','wav']
 
-import os
+def check_for_dir(path_to_check, dir_name="sounds"):
+    with os.scandir(path_to_check) as dirs_and_files:
+        sounds_dirs = [d for d in dirs_and_files if d.name == dir_name and os.path.isdir(d.path)]
+        if len(sounds_dirs) == 1:
+            return True
+        else:
+            return False
 
+# on initialize, look for a usb device that has a folder called sounds
+# for now we can give it a path... for the rpi it'll probably be /dev/sda or something like that
 
-with os.scandir("C:\Temp") as dirs:
+path_to_check = "C:\Temp"
+has_sounds = check_for_dir(path_to_check)
+
+if has_sounds:
+    print("Sounds folder found.")
+else:
+    print("Error looking for sounds folder.")
+
+valid_files = []
+invalid_files = []
+
+with os.scandir(path_to_check + "\sounds") as dirs:
     for d in dirs:
-        print(d.name[-3:])
+        if d.name[-4:] in ["." + f for f in playable_formats]:
+            valid_files.append(d)
+        else:
+            invalid_files.append(d)
+
+print("Valid files found:")
+for f in valid_files:
+    print(f.path)
+    
+print("\nOther files found:")
+for f in invalid_files:
+    print(f)    
